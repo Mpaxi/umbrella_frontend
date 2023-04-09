aaa8f031b25e3a93bd1e3a5d8edd578bf3b03884
Murilo Paxi <murilo.paxi@gmail.com>
2023-04-08 21:28:01 -0300

    Login Page
    Adicionando Bootstrap e tela de login

--- umbrella-web/src/app/auth.guard.ts
+++ umbrella-web/src/app/auth.guard.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
-import { SessaoService } from "./sessao.service";
+import { SessaoService } from './sessao.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(
-    private sessionService: SessaoService,
-    private router: Router
-  ) {}
+  constructor(private sessionService: SessaoService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
     if (this.sessionService.estaLogado()) {
       return true;
     }
 
-    return this.router.parseUrl("/login");
+    return this.router.parseUrl('/login');
   }
-
 }
